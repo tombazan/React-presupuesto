2bc29a9aaac99568c732cf88b18fd7129922f6de
tombazan <tomasbazan95@gmail.com>
2021-03-12 18:52:31 -0300

    Empezando a validar y creando mensaje de error

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,15 +1,41 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
+import Error from './Error'
 
 const Pregunta = () => {
-    return ( 
+
+    //Definir el state
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+
+    //Funcion que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value, 10))
+    }
+
+    //Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        //Validar
+        if(cantidad < 1 || isNaN(cantidad)){
+            guardarError(true);
+            return;
+        }
+        //Si pasa la validación
+        guardarError(false);
+    }
+    return (
         <Fragment>
             <h2>Coloca tu Presupuesto</h2>
 
-            <form>
+            {error ? <Error mensaje="El Presupuesto es Incorrecto" /> : null}
+
+            <form
+                onSubmit={agregarPresupuesto}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 ></input>
 
                 <input
